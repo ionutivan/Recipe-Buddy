a3508647ba9f28835f647e10944535be318c47de
ionutivan <ionut@brokoliapp.com>
2020-01-24 08:45:01 +0100

    add common classes

--- Recipe Buddy/Common/UIStoryboardExtension.swift
+++ Recipe Buddy/Common/UIStoryboardExtension.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2020 Ionut Ivan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIStoryboard {
+
+    func instantiateViewController<T: UIViewController>(ofType _: T.Type, withIdentifier identifier: String? = nil) -> T {
+        let identifier = identifier ?? String(describing: T.self)
+        return instantiateViewController(withIdentifier: identifier) as! T
+    }
+
+}
