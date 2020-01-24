97673e4c50a111d7de82fbdcfa7e5dfb11bd280d
ionutivan <ionut@brokoliapp.com>
2020-01-24 09:04:23 +0100

    add basic file structure for recipe list use case

---
+++ Recipe Buddy/RecipeList/RecipeListInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeListInteractor.swift
+//  Recipe Buddy
+//
+//  Created by Ionut Ivan on 24/01/2020.
+//  Copyright © 2020 Ionut Ivan. All rights reserved.
+//
+
+import Foundation
---
+++ Recipe Buddy/RecipeList/RecipeListPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeListPresenter.swift
+//  Recipe Buddy
+//
+//  Created by Ionut Ivan on 24/01/2020.
+//  Copyright © 2020 Ionut Ivan. All rights reserved.
+//
+
+import Foundation
