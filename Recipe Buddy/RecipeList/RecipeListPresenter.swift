5fbc71569f517218e114e11950344cf1f1ecedb2
ionutivan <ionut@brokoliapp.com>
2020-01-24 09:32:41 +0100

    add basic presenter structure

--- Recipe Buddy/RecipeList/RecipeListPresenter.swift
+++ Recipe Buddy/RecipeList/RecipeListPresenter.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+final class RecipeListPresenter {
+    
+    private unowned var userInterface: RecipeListViewProtocol
+    private let interactor: RecipeListInteractorProtocol
+    private let wireframe: RecipeListWireframeInterface
+    
+    init(wireframe: RecipeListWireframeInterface, interactor: RecipeListInteractorProtocol, UI: RecipeListViewProtocol) {
+        self.wireframe = wireframe
+        self.interactor = interactor
+        self.userInterface = UI
+    }
+}
+
+extension RecipeListPresenter: RecipeListViewInterface {}
+
+extension RecipeListPresenter: RecipeListPresenterInterface {
+
+    func viewDidLoad() {
+        fatalError("Implementation pending...")
+    }
+
+    func viewWillAppear(animated: Bool) {
+        fatalError("Implementation pending...")
+    }
+
+    func viewDidAppear(animated: Bool) {
+        fatalError("Implementation pending...")
+    }
+
+    func viewWillDisappear(animated: Bool) {
+        fatalError("Implementation pending...")
+    }
+
+    func viewDidDisappear(animated: Bool) {
+        fatalError("Implementation pending...")
+    }
+}
