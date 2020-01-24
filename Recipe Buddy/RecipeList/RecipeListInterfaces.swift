c936e2543f7a73cb9e9124765ffa8066c0e87df2
ionutivan <ionut@brokoliapp.com>
2020-01-24 09:13:45 +0100

    add basic stucture of interfaces

--- Recipe Buddy/RecipeList/RecipeListInterfaces.swift
+++ Recipe Buddy/RecipeList/RecipeListInterfaces.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+protocol RecipeListViewInterface: AnyObject {}
+
+protocol RecipeListPresenterInterface {
+    func viewDidLoad()
+    func viewWillAppear(animated: Bool)
+    func viewDidAppear(animated: Bool)
+    func viewWillDisappear(animated: Bool)
+    func viewDidDisappear(animated: Bool)
+}
+
+protocol RecipeListInteractorInterface: AnyObject {}
+
+protocol RecipeListViewProtocol: AnyObject {}
+
+enum RecipeListNavigationOption {
+    case detail
+    case favorites
+}
+
+protocol RecipeListWireframeInterface {
+    func navigate(to option: RecipeListNavigationOption)
+}
