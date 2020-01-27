aaa260722af1d5b1972bc89b170a36e6b152bbc5
ionutivan <ionut@brokoliapp.com>
2020-01-27 17:06:40 +0100

    add basic API service

--- Recipe Buddy/RecipeList/RecipeListAPIService.swift
+++ Recipe Buddy/RecipeList/RecipeListAPIService.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+final class RecipeListAPIService {
+    
+    let session = URLSession(configuration: .default)
+    
+    func getItems(for parameters: [String], page: UInt) {
+        let url = URL(string: "http://www.recipepuppy.com/api/?i=onions,garlic&p=1")!
+        let request = URLRequest(url: url)
+        print("getItems")
+        session.dataTask(with: request) { data, response, error in
+            
+            if let data = data {
+                let s = String(data: data, encoding: .utf8)!
+                print(s)
+            }
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
