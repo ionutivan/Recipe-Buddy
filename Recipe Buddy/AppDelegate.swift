36d25773d2bd5e4f97519a93e6374284be85258f
ionutivan <ionut@brokoliapp.com>
2020-01-27 15:53:08 +0100

    add recipe list storyboard and basic app delegate structure

--- Recipe Buddy/AppDelegate.swift
+++ Recipe Buddy/AppDelegate.swift
@@ -11,10 +11,18 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let initialController = UINavigationController()
+        initialController.setRootWireframe(RecipeListWireframe())
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+
+        self.window?.rootViewController = initialController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
