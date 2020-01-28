a00977384756ad3c95eff0fe36a0a79ef913849f
ionutivan <ionut@brokoliapp.com>
2020-01-28 19:59:17 +0100

    add register nib for collection view cell

--- Recipe Buddy/Common/UICollectionViewCell+Reusable.swift
+++ Recipe Buddy/Common/UICollectionViewCell+Reusable.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2020 Ionut Ivan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol CollectionViewType {
+    func register<T: UICollectionViewCell>(_ cellClass: T.Type)
+    func registerNib<T: UICollectionViewCell>(_ cellClass: T.Type)
+    func dequeue<T: UICollectionViewCell>(_ cellClass: T.Type, for indexPath: IndexPath) -> T
+}
+
+extension UICollectionView: CollectionViewType {
+    func register<T: UICollectionViewCell>(_ cellClass: T.Type) {
+        register(cellClass, forCellWithReuseIdentifier:  String(describing: cellClass))
+    }
+    
+    func registerNib<T: UICollectionViewCell>(_ cellClass: T.Type) {
+        
+        register(UINib(nibName: String(describing: cellClass), bundle: nil), forCellWithReuseIdentifier: String(describing: cellClass))
+    }
+    
+    func dequeue<T: AnyObject>(_ cellClass: T.Type, for indexPath: IndexPath) -> T {
+        return dequeueReusableCell(withReuseIdentifier: String(describing: cellClass), for: indexPath) as! T
+    }
+}
+
+// USAGE
+//let cell = collectionView.dequeue(MediaCell.self, for: indexPath)!
